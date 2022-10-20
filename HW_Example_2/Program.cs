fe156c810542454d97b8f23f66341a38429bd48b
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-10-20 21:09:44 +0300

    Create HW_Example_2

---
+++ HW_Example_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа нахождения наибольшего числа");
+Console.WriteLine("Введите первое числов");
+int number_1 = int.Parse(Console.ReadLine ()!);
+Console.WriteLine("Введите второе числов");
+int number_2 = int.Parse(Console.ReadLine ()!);
+int max = number_1;
+
+if (number_1 > max) max = number_1;
+if (number_2 > max) max = number_2;
+
+Console.Write("Наибольшее число  ");
+Console.WriteLine (max);
