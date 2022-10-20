67e0037b2622c7cac3e2fb29bb79087856c1c770
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-10-20 21:51:54 +0300

    Create HW_Example_2

--- HW_Example_2/Program.cs
+++ HW_Example_2/Program.cs
@@ -4,9 +4,19 @@ int number_1 = int.Parse(Console.ReadLine ()!);
 Console.WriteLine("Введите второе числов");
 int number_2 = int.Parse(Console.ReadLine ()!);
 int max = number_1;
+int min = number_1;
 
+if (number_1 == number_2)
+            {
+                Console.WriteLine("Числа равны");
+ 
+            }
 if (number_1 > max) max = number_1;
 if (number_2 > max) max = number_2;
-
+if (number_1 > max) min = number_1;
+if (number_2 > max) min = number_2;
+          
 Console.Write("Наибольшее число  ");
 Console.WriteLine (max);
+Console.Write("Наименьшее число  ");
+Console.WriteLine (min);
