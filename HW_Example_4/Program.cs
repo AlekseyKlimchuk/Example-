704b6c6e6b2d15a41071fd4a7b060e9b7fc87505
Aleksey <Aleksey.A.Klimchuk@gmail.com>
2022-10-20 22:21:19 +0300

    Create HW_Example_4

--- HW_Example_4/Program.cs
+++ HW_Example_4/Program.cs
@@ -6,11 +6,16 @@ int number_2 = int.Parse(Console.ReadLine ()!);
 Console.WriteLine("Введите третье число");
 int number_3 = int.Parse(Console.ReadLine ()!);
 int max = number_1;
+int min = number_1;
 
 if (number_1 > max) max = number_1;
 if (number_2 > max) max = number_2;
 if (number_3 > max) max = number_3;
-
+if (number_1 > max) min = number_1;
+if (number_2 > max) min = number_2;
+if (number_3 > max) min = number_3;
           
 Console.Write("Наибольшее число  ");
 Console.WriteLine (max);
+Console.Write("Наименьшее число  ");
+Console.WriteLine (min);
